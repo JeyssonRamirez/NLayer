202763e86fa32737acca6a807e43500c3d73082e
Jeysson Stevens R <jeysson.ramirez@outlook.com>
2013-08-29 12:10:31 -0500

    9

--- Nlayer/Infraestructura.Transversales.IoC/FabricaIoC.cs
+++ Nlayer/Infraestructura.Transversales.IoC/FabricaIoC.cs
@@ -1,8 +1,8 @@
 ﻿#region Signature
 
 //   -----------------------------------------------------------------------
-//   <copyright file=FabricaIoC.cs company="SCI Software">
-//       Copyright (c) SCI Software Todos los derechos reservados.
+//   <copyright file=FabricaIoC.cs company="Nlayer Software">
+//       Copyright (c) Nlayer Software Todos los derechos reservados.
 //   </copyright>
 //   <author>Jeysson Stevens  Ramirez </author>
 //   <Date>  2012 -02-25  - 01:48 p.m.</Date>
@@ -14,15 +14,15 @@
 #region
 
 using Microsoft.Practices.Unity;
-using Sci.Nlayer.Infraestructura.Datos.ModuloPrincipal.UnidadDeTrabajo;
-using Sci.Nlayer.Infraestructura.Transversales.Correos;
-using Sci.Nlayer.Infraestructura.Transversales.Frameworks.Correos;
-using Sci.Nlayer.Infraestructura.Transversales.Frameworks.Logging;
-using Sci.Nlayer.Infraestructura.Transversales.Logging;
+using Nlayer.Nlayer.Infraestructura.Datos.ModuloPrincipal.UnidadDeTrabajo;
+using Nlayer.Nlayer.Infraestructura.Transversales.Correos;
+using Nlayer.Nlayer.Infraestructura.Transversales.Frameworks.Correos;
+using Nlayer.Nlayer.Infraestructura.Transversales.Frameworks.Logging;
+using Nlayer.Nlayer.Infraestructura.Transversales.Logging;
 
 #endregion
 
-namespace Sci.Nlayer.Infraestructura.Transversales.IoC
+namespace Nlayer.Nlayer.Infraestructura.Transversales.IoC
 {
     public static class FabricaIoC
     {
